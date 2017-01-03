ebccda7484974c3da76865361477544c7b52d029
magdgar <magdgar@gmail.com>
2017-01-04 00:52:55 +0100

    Add user, Get user endpoints

--- service/Bookcase.py
+++ service/Bookcase.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from flask.ext.restplus import abort
 
@@ -11,22 +11,32 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = [user for user in users if user['id'] == user_id]
+    if len(user) == 0:
+        abort(404)
+    return jsonify({"user": user[0]})
+
+
 @app.route('/user', methods=['POST'])
 def add_user():
     if not request.json:
         abort(400)
 
     user = {
+        'id': users.__len__() + 1,
         'name': request.json['name'],
         'email': request.json['email']
     }
     users.append(user)
-    return str(user)
+    return jsonify({"user": user})
 
 
-@app.route('/users', methods=['GET'])
+@app.route('/all_users', methods=['GET'])
 def hello():
     return str(users)
+
 
 if __name__ == '__main__':
     app.run()
