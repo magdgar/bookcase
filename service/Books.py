0a9137474bdf08bf0e5d8f33bd7f199eb4a95b19
magdgar <magdgar@gmail.com>
2017-01-13 00:02:40 +0100

    getting optional values from post json

--- service/Books.py
+++ service/Books.py
@@ -5,22 +5,22 @@ from flask import Blueprint, jsonify, request
 simple_page = Blueprint('simple_page', __name__, template_folder='templates')
 books = []
 
+
 @simple_page.route('/book', methods=['GET'])
 def get_book():
     return jsonify({"book": "boook"})
 
 
-@simple_page.route('/user', methods=['POST'])
-def add_user():
+@simple_page.route('/add_book', methods=['POST'])
+def add_book():
     if not request.json:
         abort(400)
 
-    user = {
+    book = {
         'id': books.__len__() + 1,
         'author': request.json['author'],
         'title': request.json['title'],
-        'IBN': request.json['IBN'],
-        'publication_date': request.json['publication_date']
+        'publication_date': request.json.get('publication_date')
     }
-    books.append(user)
-    return jsonify({"user": user})
+    books.append(book)
+    return jsonify({"book": book})
