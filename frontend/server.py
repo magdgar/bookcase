8d1ab277eb3ecc298b8ae73f74ec3de8ad072555
magdgar <magdgar@gmail.com>
2017-02-11 21:06:55 +0100

    Clean server.py file

--- frontend/server.py
+++ frontend/server.py
@@ -1,20 +1,11 @@
-import os
-
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='template')
-
-#
-# settings.configure(
-#     RENDER=True,
-#     RENDER_URL='http://127.0.0.1:9009/render'
-# )
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
